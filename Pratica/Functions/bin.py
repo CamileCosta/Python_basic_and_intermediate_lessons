ceb2c978d3a9c4e132310e2ee36f99002a3b697e
CamileCosta <dreamerdsm@gmail.com>
2025-10-13 22:13:13 -0300

    update all

---
+++ Pratica/Functions/bin.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um número: '))
+print('I love doing this!')
+print(max(9, 8))
+print(min(7, 5))
+
+cinco_em_binario = bin(5) #retorna o binário do número inserido
+print(cinco_em_binario)
--- Pratica/bin.py
+++
@@ -1,2 +0,0 @@
-cinco_em_binario = bin(5) #retorna o binário do número inserido
-print(cinco_em_binario)
