3a2f9b093b719da508218c1b90743997d5eeba0d
CamileCosta <dreamerdsm@gmail.com>
2025-10-15 18:32:11 -0300

    more exercise

---
+++ Intermediate Python/tuples-ex.py
@@ -0,0 +1,8 @@
+pg = ('-23.998894', '-46.413544')
+lo = ('51.507351', '-0.127758')
+pa = ('48.856613', '2.352222')
+
+print("Latitude and Longitude of the furthest friend's location:", pg)
+
+all = pg + lo + pa
+print("All friends' locations combined: ", all)
