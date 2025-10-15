52732aa24c04cbb2b48f38ac44b07a77bd2910e9
CamileCosta <dreamerdsm@gmail.com>
2025-10-15 18:57:03 -0300

    more exercise

---
+++ Intermediate Python/dictionary-ex.py
@@ -0,0 +1,15 @@
+Sabrina = {
+    'name': 'Sabrina',
+    'artist': 'Samuel Palmer',
+    'nationality': 'British',
+    'date': '1856'
+}
+
+print('An artwork from the met museum site!')
+print('Name:', Sabrina['name'])
+print('Artist:', Sabrina['artist'])
+print('Nationality:', Sabrina['nationality'])
+print('Date:', Sabrina['date'])
+
+print('Keys:', Sabrina.keys()) # If you want to print just the keys
+print('values:', Sabrina.values()) # If you want to print just the values
